a27bdcff5b538a53f4a6854d2445023a8497b296
Dilan <dio4@pitt.edu>
2020-09-28 19:59:18 -0400

    Integration builder build folder

---
+++ integration-builder/build/precache-manifest.022053aceba40a852e29f3b396bf2da8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68574c8d3bea393dba726dc5bf87da07",
+    "url": "./index.html"
+  },
+  {
+    "revision": "18692c09ccbc73577fcf",
+    "url": "./static/css/2.1e1a7013.chunk.css"
+  },
+  {
+    "revision": "b940e6803be1ae8a212a",
+    "url": "./static/css/main.c9c5c8ac.chunk.css"
+  },
+  {
+    "revision": "18692c09ccbc73577fcf",
+    "url": "./static/js/2.6e4709df.chunk.js"
+  },
+  {
+    "revision": "f99101bcbb881ebb48d85fa1812e472b",
+    "url": "./static/js/2.6e4709df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b940e6803be1ae8a212a",
+    "url": "./static/js/main.c6468a88.chunk.js"
+  },
+  {
+    "revision": "467144f25f461390a54b",
+    "url": "./static/js/runtime-main.6613df9e.js"
+  }
+]);
