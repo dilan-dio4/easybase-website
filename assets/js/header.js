c18f56854abdf7ae7d417fe62497613ed1b759a3
Dilan <dio4@pitt.edu>
2021-01-10 12:51:53 -0500

    new status symbole

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,3 +1,8 @@
+function toggleMobileNav() {
+    document.getElementsByClassName("mobile-nav")[0].classList.toggle("visible");
+    document.body.classList.toggle("overflow-hidden");
+}
+
 window.addEventListener("load", function () {
 
     document.getElementById('banner_close').addEventListener('click', function () {
@@ -5,29 +10,34 @@ window.addEventListener("load", function () {
         document.documentElement.style.setProperty('--banner-height', "0px");
     });
 
-    async function pingTimeWorker() {
+    async function statusWorker() {
         try {
-            const ping_res = await fetch('https://api.easybase.io/pingResponseTime');
-            const num = await ping_res.text();
-            Array.from(document.getElementsByClassName("ping-time-text")).forEach(ele => ele.innerHTML = `${num}ms`);
-            setTimeout(pingTimeWorker, 4500);
+            const status_res = await fetch('https://8x8pb0hcdvq9.statuspage.io/api/v2/status.json');
+            const status_json = await status_res.json();
+
+            if (status_json.status.indicator === "none") {
+                // Already set in styles.css
+                // document.querySelector(':root').style.setProperty('--flash-container-bg', "#22e2298c");
+                // document.querySelector(':root').style.setProperty('--flasher-bg', "var(--mod-cyan-darker)");
+                // Array.from(document.getElementsByClassName("sys-status-text")).forEach(ele => ele.innerHTML = "Operational");
+            } else if (status_json.status.indicator === "minor") {
+                document.querySelector(':root').style.setProperty('--flash-container-bg', "var(--mod-warning-light)");
+                document.querySelector(':root').style.setProperty('--flasher-bg', "var(--mod-warning)");
+                Array.from(document.getElementsByClassName("sys-status-text")).forEach(ele => ele.innerHTML = "Minor Issues");
+            } else if (status_json.status.indicator === "major" || status_json.status.indicator === "critical") {
+                document.querySelector(':root').style.setProperty('--flash-container-bg', "var(--mod-error)");
+                document.querySelector(':root').style.setProperty('--flasher-bg', "var(--mod-error-light)");
+                Array.from(document.getElementsByClassName("sys-status-text")).forEach(ele => ele.innerHTML = "System Error");
+            }
         } catch (error) {
-            console.log("Couldn't fetch pingResponseTime");
+            console.log("Couldn't fetch statusWorker", error);
         }
+    
     }
-    pingTimeWorker();
+    statusWorker();
 
-
-    document.getElementsByClassName("mobile-menu_indicator")[0].addEventListener('click', function () {
-        document.getElementsByClassName("mobile-nav")[0].classList.toggle("visible");
-        document.body.classList.toggle("overflow-hidden");
-    }, false);
-
-    document.getElementById("icon-close-mobile").addEventListener('click', function () {
-        document.getElementsByClassName("mobile-nav")[0].classList.toggle("visible");
-        document.body.classList.toggle("overflow-hidden");
-    }, false);
-
+    document.getElementsByClassName("mobile-menu_indicator")[0].addEventListener('click', toggleMobileNav, false);
+    document.getElementById("icon-close-mobile").addEventListener('click', toggleMobileNav, false);
 
     var getScrollY = function () { return (window.pageYOffset || window.document.documentElement.scrollTop) - (window.document.documentElement.clientTop || 0); };
     var getWindowWidth = function () { return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth; };
